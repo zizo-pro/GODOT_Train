bc08079c288564947f56350c7d48877980440b1a
wally <wallyallahninja@gmail.com>
2022-04-29 21:34:57 +0200

    added some player animation,edited in some test map

--- Knight.gd
+++ Knight.gd
@@ -1,23 +1,44 @@
 extends KinematicBody2D
 
-var motion = Vector2()
-var speed = 2.6
+var velocity = Vector2(0,0)
+const SPEED = 180
+const GRAVITY = 15
+const JUMPFORCE = -350
+const CROUCH_SPEED = 100
 
 func _physics_process(delta):
-	# Move Right +x
-	if Input.is_action_pressed("ui_right"):
-		position.x += speed
-
-	# Move Left -x
-	if Input.is_action_pressed("ui_left"):
-		position.x -= speed
-	
-	# Move Up -y
-	if Input.is_action_pressed("ui_up"):
-		position.y -= speed
-	
-	# Move Down +y
-	if Input.is_action_pressed("ui_down"):
-		position.y += speed
+	# Move Right (+x)
+	if Input.is_action_pressed("D_Key"):
+		velocity.x = SPEED
+		$Sprite.play("Running")
 		
+	# Move Left (-x)
+	elif Input.is_action_pressed("A_Key"):
+		velocity.x = -SPEED
+		$Sprite.play("Running")
+		
+	else:
+		$Sprite.play("Idle")
+	
+	if not is_on_floor() and velocity.y < 0:
+		$Sprite.play("Jump")
+		
+	if not is_on_floor() and velocity.y > 0:
+		$Sprite.play("Falling")
+		
+	#Falling Speed Increase by Time
+	velocity.y = velocity.y + GRAVITY
+		
+	#Jump
+	if Input.is_action_just_pressed("Space_Key"):# and is_on_floor():
+		velocity.y = JUMPFORCE
+		
+	velocity = move_and_slide(velocity,Vector2.UP)
+	#Slide After Releasing Move Keys
+	velocity.x = lerp(velocity.x,0,0.5)
+
+func _on_crouch():
 	pass
+
+
+
